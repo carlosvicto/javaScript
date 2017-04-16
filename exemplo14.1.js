aac84825c62fe55b86a380431fd7071f8692e753
carlosvicto <cvvictor4@live.com>
2017-04-16 10:35:09 -0300

    Add files via upload

---
+++ exemplo14.1.js
@@ -0,0 +1,9 @@
+function evento_entrar(){
+ alert('Seja Bem Vindo');
+}
+function evento_sair(){
+ alert('Até a Próxima Visita');
+}
+function evento_clique(){
+ document.getElementById('id2').style.color='red';
+}
